b5a25ad467a0f7c7d7ee451bb51fb165560ec806
Piotr Piatrovich <piotr.piatrovich@sharekey.com>
2026-09-25 15:10:59 +0300

    fix refresh button for external

--- wrapper/v1/wrapper-mount.js
+++ wrapper/v1/wrapper-mount.js
@@ -2389,7 +2389,14 @@
               redoButton.style.display = 'none';
               slideshowButton.style.display = 'none';
 
-              return mountDownloadButton(doc) && (!shouldMountMainAppButton || mountMainAppButton(doc));
+              var externalEditDone = mountEditButton(doc);
+
+              mountEditingLabel(doc);
+
+              var externalDownloadDone = mountDownloadButton(doc);
+              var externalMainAppDone = !shouldMountMainAppButton || mountMainAppButton(doc);
+
+              return externalEditDone && externalDownloadDone && externalMainAppDone;
           }
 
           undoButton.querySelector('button').innerHTML = '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">' +
